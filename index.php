b2bd9998274474cd3b5073aa53e361825cbabf5b
caldwell-matthew <caldwell.j.matthew@gmail.com>
2022-11-18 17:30:07 -0500

    Established connection to database.

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-    echo "HELLO WORLD"
+  // Database Info
+  $servername = 'us-cdbr-east-06.cleardb.net';
+  $username = 'be21fda1d3f174';
+  $password = '4d1817db';
+  $dbname = 'heroku_2e91707667d2cbd';
+
+  // PHP mySQL Connector
+  $SQLcon = new mysqli($servername, $username, $password, $dbname);
+
+  if($SQLcon -> connect_errno) {
+    die("mySQL CONNECTION FAILURE: ".$SQLcon -> connect_errno);
+  }
+  else echo "mySQL CONNECTION ACTIVE!\n";
 ?>
