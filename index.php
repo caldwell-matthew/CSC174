659bab2ca41c35c265e42737c41007d1580b30cd
caldwell-matthew <caldwell.j.matthew@gmail.com>
2022-11-28 19:22:38 -0500

    12

--- index.php
+++ index.php
@@ -100,13 +100,51 @@
         <option value=45>45 g</option>
         <option value=50>50 g</option>
       </select>
-      <br><br>
+      <br>
+      <br>
       <input type = "submit" value = "submit">
     </form>
   </body>
-  <br><br>
   <?php
-    // Values from index.html form to be inserted.
+    // Database Info
+    // mysql://b39bd7484b29d1:e63a8cc1@us-cdbr-east-06.cleardb.net/heroku_9a0b3385c551174?reconnect=true
+    $servername = 'us-cdbr-east-06.cleardb.net';
+    $username = 'b39bd7484b29d1';
+    $password = 'e63a8cc1';
+    $dbname = 'heroku_9a0b3385c551174';
+
+    // PHP mySQL Connector
+    $SQLcon = new mysqli($servername, $username, $password, $dbname);
+
+    if($SQLcon -> connect_errno) 
+      die("mySQL CONNECTION FAILURE: ".$SQLcon -> connect_errno);
+    else 
+      echo "mySQL CONNECTION ACTIVE!\n\n";
+
+    // Prepared statement for inserting data into drink table.
+    $insert = $SQLcon -> prepare(
+      "INSERT INTO drink 
+      (drink_id, name, size, calories, caffeine, sugar_qty) 
+      VALUES (?, ?, ?, ?, ?, ?)");
+    $insert -> bind_param("isiiii", $drink_id, $name, $size, $calories, $caffeine, $sugar_qty);
+
+    // $insert -> execute();
+    // $insert -> close();
+
+    // Prepared statement for selecting all data from drink table.
+    $select = $SQLcon -> prepare("SELECT * FROM drink");
+    $result = $select -> execute();
+    $result = $select -> get_result(); 
+    
+    foreach ($result as $row) {
+      print_r($row);
+      print_r("\n");
+    }
+
+    $select -> close();
+    $SQLcon -> close();
+
+    // Values from form to be inserted.
     $drink_id = 0;
     $name= $_POST['dname'];
     $size = $_POST["size"];
